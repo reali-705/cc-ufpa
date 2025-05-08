153b227ff039448e947dd94ed3d5e19a641538b5
Ale-Reali-705 <alessandro.reali.3110@gmail.com>
2025-05-08 11:55:37 -0300

    atividade 1 - explicada em sala

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # TRABALHO DE PROJETO DE ALGORITMOS I
+## ATIVIDADE 1
+criando uma página com html e JavaScript  
+Adicionando uma foto e uma descrição do jogo escolhido
---
+++ pagina_jogo.js
@@ -0,0 +1,7 @@
+function output(t) {
+    document.write('<p>' + t + '</p>');
+}
+  
+alert('Bem vindo ao jogo Angry Birds!'); // texto de alerta
+console.log('Bem vindo ao jogo Angry Birds!'); // texto de console
+output('Bem vindo ao jogo Angry Birds!'); // texto da funcao output, adiciona um paragrafo a pagina
