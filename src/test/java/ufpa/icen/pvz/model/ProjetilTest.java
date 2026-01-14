f3ba43c72922969bd44adcadbc1032a3fb455a51
reali-705 <alessandro.reali.3110@gmail.com>
2026-01-14 11:50:51 -0300

    feat(test): adiciona testes para a classe Projetil

---
+++ src/test/java/ufpa/icen/pvz/model/ProjetilTest.java
@@ -0,0 +1,25 @@
+package ufpa.icen.pvz.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ProjetilTest {
+    
+    private Projetil projetil;
+    private static final double POSICAO_INICIAL_X = 9.0;
+    private static final double POSICAO_INICIAL_Y = 0;
+
+    @BeforeEach
+    public void setUp() {
+        projetil = new Projetil(Config.DANO_ATIRADOR, Config.VELOCIDADE_PROJETIL, POSICAO_INICIAL_X, POSICAO_INICIAL_Y);
+    }
+
+    @Test
+    public void testMovimento() {
+        projetil.mover();
+        double posicaoEsperada = POSICAO_INICIAL_X + Config.VELOCIDADE_PROJETIL;
+        assertEquals(posicaoEsperada, projetil.getPosicaoX(), 0.01, "A posição X do projetil após o movimento está incorreta.");
+        assertEquals(POSICAO_INICIAL_Y, projetil.getPosicaoY(), 0.01, "A posição Y do projetil deve permanecer inalterada.");
+    }
+}
