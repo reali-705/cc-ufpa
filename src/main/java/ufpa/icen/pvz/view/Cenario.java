a3f6f9ecde1223e050c2b57f44f9402a64304c42
FelipeBrasill <felipebrasil567@gmail.com>
2026-01-17 20:50:55 -0300

    Configuração inicial do Frontend (#1) adiciona as classes Cenario e Janela. Cria o primeiro sprite do fundo e a criação das dependencias resources e assets
    Close #3

---
+++ src/main/java/ufpa/icen/pvz/view/Cenario.java
@@ -0,0 +1,20 @@
+package ufpa.icen.pvz.view;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.IOException;
+
+public class Cenario extends JPanel{
+    BufferedImage imagemFundo;
+    public Cenario() {
+    try {
+        imagemFundo = ImageIO.read(
+            getClass().getResourceAsStream("/assets/fundo.png")
+        );
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+}
+}
