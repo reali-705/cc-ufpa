115703150ba7615bfb7a5e0d7ebaced69f09e5cc
FelipeBrasill <felipebrasil567@gmail.com>
2026-01-17 20:50:55 -0300

    Configuração inicial do Frontend (#1) adiciona as classes Cenario e Janela. Cria o primeiro sprite do fundo e a criação das dependencias resources e assets
    Close #3

--- src/main/java/ufpa/icen/pvz/view/GameView.java
+++ src/main/java/ufpa/icen/pvz/view/GameView.java
@@ -1,5 +1,23 @@
 package ufpa.icen.pvz.view;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class GameView {
-    
+    private JFrame telajogo;
+
+    public GameView() {
+        // Cria a janela principal
+        telajogo = new JFrame("Plants vs Zombies");
+        telajogo.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        telajogo.setSize(800, 600);
+        telajogo.setVisible(true);
+        telajogo.setLocationRelativeTo(null);
+
+    }
+
+    // Exemplo de uso (remova depois de testar)
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new GameView());
+    }
 }
---
+++ src/main/java/ufpa/icen/pvz/view/Janela.java
@@ -0,0 +1,18 @@
+package ufpa.icen.pvz.view;
+import javax.swing.JFrame;
+// setSize(960, 720);
+
+public class Janela extends JFrame{
+    Cenario cenario;
+    public Janela(String titulo, int largura, int altura){
+        // Configurações básicas da janela
+        super(titulo);
+        setSize(largura, altura);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        this.cenario = new Cenario();
+        add(cenario);
+    }
+    
+}
