1ab1c4194cceb756f8a05780df3f377c2d310dad
Alessandro Reali <alessandro.reali.3110@gmail.com>
2026-01-18 11:34:32 -0300

    test: Refactor image loading in Cenario constructor
    *testando funcionalidade*
    
    Altera formatação

--- src/main/java/ufpa/icen/pvz/view/Cenario.java
+++ src/main/java/ufpa/icen/pvz/view/Cenario.java
@@ -1,4 +1,5 @@
 package ufpa.icen.pvz.view;
+
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -9,12 +10,12 @@ import java.io.IOException;
 public class Cenario extends JPanel{
     BufferedImage imagemFundo;
     public Cenario() {
-    try {
-        imagemFundo = ImageIO.read(
-            getClass().getResourceAsStream("/assets/fundo.png")
-        );
-    } catch (IOException e) {
-        e.printStackTrace();
+        try {
+            imagemFundo = ImageIO.read(
+                getClass().getResourceAsStream("/assets/fundo.png")
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-}
 }
