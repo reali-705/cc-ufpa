64d21a0fb1657cef70bcd2952fae4620fdf097c6
reali-705 <alessandro.reali.3110@gmail.com>
2026-02-16 15:25:03 -0300

    feat: desenvolvimento do menu básico

--- atividade_3/main.py
+++ atividade_3/main.py
@@ -2,11 +2,42 @@
 Módulo para executar a implementação do algoritmo Shift-And Aproximado.
 """
 
+import util
+from shift_and_aproximado import buscar
+
 
 def main():
     """
     Função principal do módulo main.
     """
+    while True:
+        tipo_texto = util.menu_principal()
+        match tipo_texto:
+            case 0:
+                print("Saindo do programa...")
+                return
+            case 1:
+                texto = util.gerar_texto_aleatorio(util.escolher_tamanho_texto())
+            case 2:
+                texto = util.gerar_texto_com_padrao(
+                    util.escolher_tamanho_texto(),
+                    input("Digite o padrão a ser inserido no texto: "),
+                )
+            case _:
+                print("Opção inválida. Por favor, tente novamente.")
+                continue
+
+        print(f"Texto gerado:\n{texto}\n")
+
+        k = util.escolher_k()
+        padrao = input("Digite o padrão de busca: ")
+
+        # Buscar o padrão no texto usando o algoritmo Shift-And Aproximado
+        resultados = buscar(texto, padrao, k)
+        if resultados:
+            print(f"Padrão encontrado nas posições: {resultados}")
+        else:
+            print("Padrão não encontrado no texto.")
 
 
 if __name__ == "__main__":
--- atividade_3/util.py
+++ atividade_3/util.py
@@ -45,3 +45,79 @@ def gerar_texto_com_padrao(tamanho: int, padrao: str) -> str:
     texto_com_padrao = texto[:indice] + padrao + texto[indice:]
 
     return texto_com_padrao
+
+
+def gerar_menu_opcoes(opcoes: list[str]) -> str:
+    """
+    Gera um menu de opcoes a partir de uma lista de opções.
+
+    Args:
+        opcoes (list[str]): Uma lista de opções para o menu.
+
+    Returns:
+        str: Uma string formatada representando o menu.
+    """
+    opcoes = list(map(str.strip, opcoes))
+    if "Sair" not in opcoes:
+        opcoes.insert(0, "Sair")
+    return "\n".join(f"({i})  {opcao}" for i, opcao in enumerate(opcoes))
+
+
+def escolher_tamanho_texto() -> int:
+    """
+    Função para escolher o tamanho do texto a ser gerado.
+
+    Returns:
+        int: O tamanho do texto escolhido pelo usuário.
+    """
+    opcoes = ["500", "1000", "1500", "2000", "3000"]
+    while True:
+        try:
+            print("Escolha o tamanho do texto:")
+            print(gerar_menu_opcoes(opcoes))
+            escolha = int(input("Digite o número correspondente ao tamanho do texto: "))
+
+            if escolha < 0 or escolha > len(opcoes):
+                raise ValueError("Escolha inválida. Por favor, tente novamente.")
+            return int(opcoes[escolha - 1])
+        except ValueError as e:
+            print(f"Erro: {e}\n")
+
+
+def escolher_k() -> int:
+    """
+    Função para escolher o número máximo de erros permitidos.
+
+    Returns:
+        int: O número máximo de erros permitido escolhido pelo usuário.
+    """
+    while True:
+        try:
+            k = int(input("Escolha o número máximo de erros permitidos (máximo 2): "))
+            if k < 0 or k > 2:
+                raise ValueError("O número de erros deve ser um inteiro entre 0 e 2.")
+            return k
+        except ValueError as e:
+            print(f"Erro: {e}\n")
+
+
+def menu_principal() -> int:
+    """
+    Docstring para menu_principal
+
+    :return: Descrição
+    :rtype: int
+    """
+    while True:
+        try:
+            print("Escolha uma opção:")
+            print(
+                gerar_menu_opcoes(["Gerar texto aleatório", "Gerar texto com padrão"])
+            )
+            escolha = int(input("Digite o número correspondente à opção desejada: "))
+
+            if escolha < 0 or escolha > 2:
+                raise ValueError("Escolha inválida. Por favor, tente novamente.")
+            return escolha
+        except ValueError as e:
+            print(f"Erro: {e}\n")
